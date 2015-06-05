f4f784723b7f448410169ad463ca6fbb149ae97e
Pawel Duda <pawelduda91@gmail.com>
2015-06-05 22:28:48 +0200

    Polish solution #199

--- easy/199.rb
+++ easy/199.rb
@@ -14,14 +14,14 @@ def parse_input(input)
   }
 end
 
-# File.open(ARGV[0]).each_line do |word_and_mask|
-#   args = parse_input(word_and_mask)
-#   puts mask_word(args[:word], args[:mask])
-# end
+File.open(ARGV[0]).each_line do |word_and_mask|
+  args = parse_input(word_and_mask)
+  puts mask_word(args[:word], args[:mask])
+end
 
 # tests
-p mask_word('hello', '11001') == 'HEllO'
-p mask_word('world', '10000') == 'World'
-p mask_word('cba', '111') == 'CBA'
-args = parse_input('qngagtfhmqw 01100101111')
-puts mask_word(args[:word], args[:mask])
+# p mask_word('hello', '11001') == 'HEllO'
+# p mask_word('world', '10000') == 'World'
+# p mask_word('cba', '111') == 'CBA'
+# args = parse_input('qngagtfhmqw 01100101111')
+# puts mask_word(args[:word], args[:mask])
