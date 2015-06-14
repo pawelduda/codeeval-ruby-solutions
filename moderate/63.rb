62e119627ef3711bae84d01d1167dd9020191175
Pawel Duda <pawelduda91@gmail.com>
2015-06-14 22:01:09 +0200

    Polish solution #63

--- moderate/63.rb
+++ moderate/63.rb
@@ -19,11 +19,11 @@ def primes_in_range(from, to)
   primes_in_range.length
 end
 
-# File.open(ARGV[0]).each_line do |line|
-#   input = parse_input(line)
-#   puts primes_in_range(input[:from], input[:to])
-# end
+File.open(ARGV[0]).each_line do |line|
+  input = parse_input(line)
+  puts primes_in_range(input[:from], input[:to])
+end
 
 # tests
-p primes_in_range(2, 10) == 4
-p primes_in_range(20, 30) == 2
+# p primes_in_range(2, 10) == 4
+# p primes_in_range(20, 30) == 2
