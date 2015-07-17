d44e20d7f0dedc75a23ff93fb8e04d802bb23a00
Pawel Duda <pawelduda91@gmail.com>
2015-07-17 23:19:02 +0200

    Add hard solution #14

---
+++ hard/14.rb
@@ -0,0 +1,14 @@
+# https://www.codeeval.com/browse/14/
+
+def string_permutations(stringish)
+  stringish.split('').permutation.map { |str| str.join }.sort.join(',')
+end
+
+File.open(ARGV[0]).each_line do |line|
+  puts string_permutations(line.strip)
+end
+
+# tests
+# p string_permutations('hat') == 'aht,ath,hat,hta,tah,tha'
+# p string_permutations('abc') == 'abc,acb,bac,bca,cab,cba'
+# p string_permutations('Zu6') == '6Zu,6uZ,Z6u,Zu6,u6Z,uZ6'
